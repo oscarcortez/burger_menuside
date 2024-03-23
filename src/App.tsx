3addab0740948a51f97b22545f5730922e388e82
oscar-cortez-home <oscarkortez@gmail.com>
2024-03-23 17:12:52 -0400

    added routes with example routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
-import "./App.css";
-import "./styles.css";
+import './App.css'
+import './styles.css'
 
-// import { AiFillFire } from "react-icons/ai";
+import { Route, Routes } from 'react-router-dom'
 
-import { NavBar } from "./components/NavBar";
+import { NavBar } from './components/NavBar'
 // import { MenuItemCard } from "./components/MenuItemCard";
 
 function App() {
   return (
     <div>
       <NavBar />
-      
+      <Routes>
+        <Route path="/" element={<h1>home</h1>} />
+        <Route path="about" element={<h1>about</h1>} />
+        <Route path="services" element={<h1>services</h1>} />
+        <Route path="contact" element={<h1>contact</h1>} />
+      </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
