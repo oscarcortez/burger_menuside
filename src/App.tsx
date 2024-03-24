17668c86476bda682aa0a6fae8baa449c13aaa83
mauudev <mtrigo143@gmail.com>
2024-03-24 16:32:20 -0400

    WIP sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,35 @@ import "./styles.css";
 
 import { NavBar } from "./components/NavBar";
 import { Header } from "./components/Header/Header";
+import Sidebar from "./samples/SideBar";
+import SideBarEx from "./samples/SideBarEx";
+import OtherSideBar from "./samples/OtherSideBar";
+
 // import { MenuItemCard } from "./components/MenuItemCard";
 
 export default () => {
   return (
-    <div className="w-full flex flex-col sm:flex-row flex-wrap sm:flex-nowrap py-4 flex-grow">
-      <div className="bg-gray-100 w-fixed w-[40%] flex-shrink flex-grow-0 px-4 ">
-        {/* Ads here */}
-      </div>
-      <Header>
-        <NavBar />
-      </Header>
-      <div className="bg-gray-100 w-[40%] w-fixed flex-shrink flex-grow-0 px-2">
-        {/* Ads here */}
-      </div>
+    // <SideBarEx />
+    <div className="fixed top-0 left-0 right-0">
+      <nav className="container flex items-center justify-between py-1 lg:py-5">
+        <Header>
+         <NavBar />
+       </Header>
+      </nav>
     </div>
+
+    // <div className="w-full flex flex-col sm:flex-row flex-wrap sm:flex-nowrap py-4 flex-grow">
+    //   <div className="bg-gray-100 w-fixed w-[40%] flex-shrink flex-grow-0 px-4 ">
+    //     {/* Ads here */}
+    //   </div>
+    //   {/* <Header>
+    //     <NavBar />
+    //   </Header> */}
+    //   {/* <Sidebar /> */}
+    //   <div className="bg-gray-100 w-[40%] w-fixed flex-shrink flex-grow-0 px-2">
+    //     {/* Ads here */}
+    //   </div>
+    // </div>
   );
 }
 
