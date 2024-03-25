3f2a399486cf0917ad4327cc7d8d0d24774526eb
oscar-cortez-home <oscarkortez@gmail.com>
2024-03-25 17:47:41 -0400

    fixed anonimous function error

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,38 @@
-import "./App.css";
+import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { AiOutlineMenu } from 'react-icons/ai'
+
+import { NavBar } from './components/NavBar'
+import { MainContent } from './components/MainContent'
+import { RightSidebar } from './components/RightSidebar'
+import { ListItemCard } from './components/ListItemCard'
+import { Accordion } from './components/Accordion'
+import { ProfileCard } from './components/ProfileCard'
+
+import {
+  items,
+  seeMoreItems,
+  profileValues
+} from './service/SidebarItemsService'
+import { listRoutes } from './service/RoutesService'
+
+import './App.css'
 // import "./styles.css";
 
-import { Route, Routes } from "react-router-dom";
-
-import { NavBar } from "./components/NavBar";
-import { MainContent } from "./components/MainContent";
-import { AiOutlineMenu } from "react-icons/ai";
-import { useState } from "react";
-import { RightSidebar } from "./components/RightSidebar";
-import { ListItemCard } from "./components/ListItemCard";
-import { Accordion } from "./components/Accordion";
-import { ProfileCard } from "./components/ProfileCard";
-
-import { items, seeMoreItems, profileValues } from "./service/SidebarItemsService";
-import { listRoutes } from "./service/RoutesService";
-
-export default () => {
-  const [isVisibleRightSidebar, setIsVisibleRightSidebar] = useState<boolean>(false);
+const App = () => {
+  const [isVisibleRightSidebar, setIsVisibleRightSidebar] =
+    useState<boolean>(false)
   return (
     <div className="flex flex-row h-screen relative">
       <div className="md:w-1/3 relative border"></div>
       <MainContent>
         <NavBar
           rightMenuIcon={<AiOutlineMenu size={30} />}
-          onClickRightMenuIcon={() => { setIsVisibleRightSidebar(true) }}
+          onClickRightMenuIcon={() => {
+            setIsVisibleRightSidebar(true)
+          }}
         />
-        <RightSidebar
-          isVisible={isVisibleRightSidebar}
-        >
+        <RightSidebar isVisible={isVisibleRightSidebar}>
           <ProfileCard
             avatarImage={profileValues.avatarImage}
             profileName={profileValues.name}
@@ -46,7 +51,8 @@ export default () => {
         </Routes>
       </MainContent>
       <div className="md:w-1/3 relative border"></div>
-    </div >
-    // <ThreeColumnsPage />
-  );
-};
+    </div>
+  )
+}
+
+export default App
