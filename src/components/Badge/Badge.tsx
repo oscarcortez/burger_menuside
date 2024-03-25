406fc3cea97c040bf818de7e6d5072971caafdbf
oscar-cortez-home <oscarkortez@gmail.com>
2024-03-24 20:08:53 -0400

    fixed vertical space

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -12,7 +12,7 @@ export const Badge: FunctionComponent<BadgeProps> = ({
 }: BadgeProps): ReactNode => {
   return (
     <NavLink to={navigateTo || '/'}>
-      <div className="bg-yellow-200 text-black text-xs rounded-full p-3 float-left mt-2 mb-2">
+      <div className="bg-yellow-200 text-black text-xs rounded-full p-3 float-left mb-2">
         {label}
       </div>
     </NavLink>
