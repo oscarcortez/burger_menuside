1f238a0565fed6c3cdc8f461a141143fd415fb1d
oscar-cortez-home <oscarkortez@gmail.com>
2024-03-24 20:23:09 -0400

    added tailwind postcss generated file, implemented

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './styles.css' // Import your styles
+// import './styles.css' // Import your styles
+import './app/css/tailwind.generated.css'
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
